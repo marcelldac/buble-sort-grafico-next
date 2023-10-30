581d258e569cbb90655fbabc168ba9de28aa6863
marcelldac <dacmarcell@gmail.com>
2023-10-30 12:06:14 -0300

    feat: add InputMap component verification

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useRef, useState } from "react";
-import { Flex, Button, Input, Box, Space } from "@mantine/core";
+import { Flex, Button, Input, Box, Space, ActionIcon } from "@mantine/core";
+import { AiFillGithub } from "react-icons/ai";
+import Link from "next/link";
 
 type ArrayState = number[];
 type SortHistoryState = number[][];
@@ -8,6 +10,7 @@ export default function Home() {
   const [input, setInput] = useState<ArrayState>([]);
   const [sortHistory, setSortHistory] = useState<SortHistoryState>([[]]);
   const [playing, setPlaying] = useState<Boolean>(false);
+  const [isNumber, setIsNumber] = useState<Boolean>(true);
   const [looping, setLooping] = useState<number>(0);
   const timeOutRef = useRef<NodeJS.Timeout | null>(null);
 
@@ -36,7 +39,13 @@ export default function Home() {
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     let value = e.target.value;
-    setInput(value.split(",").map((i) => Number(i)));
+    setInput(
+      value.split(",").map((i) => {
+        const number = Number(i);
+        setIsNumber(!isNaN(number));
+        return number;
+      })
+    );
   };
 
   const bubbleSort = (): void => {
@@ -55,18 +64,9 @@ export default function Home() {
     }
   };
 
-  return (
-    <Flex direction="column" justify="center" align="center" h="100vh">
-      <Box>
-        <Input.Wrapper label="Type an array following the example. Separate with comma.">
-          <Input type="text" onChange={handleChange} placeholder="Ex.: 3,2,1" />
-        </Input.Wrapper>
-        <Space h="md" />
-        <Button fullWidth onClick={bubbleSort}>
-          Bubble Sort!
-        </Button>
-      </Box>
-      <Flex direction="row">
+  const InputMap = () => {
+    return (
+      <>
         {input.map((element, key) => (
           <>
             <Box key={key} size="xl" component="h3">
@@ -75,7 +75,40 @@ export default function Home() {
             <Space w="md" />
           </>
         ))}
+      </>
+    );
+  };
+
+  return (
+    <Flex direction="column" justify="center" align="center" h="100vh">
+      <Box>
+        <Input.Wrapper label="Type an array following the example. Separate with comma.">
+          <Input
+            required
+            type="text"
+            onChange={handleChange}
+            placeholder="Ex.: 3,2,1"
+          />
+        </Input.Wrapper>
+        <Space h="md" />
+        <Button fullWidth onClick={bubbleSort}>
+          Bubble Sort!
+        </Button>
+      </Box>
+      <Flex direction="row">
+        {isNumber && <InputMap />}
+        <Space h="md" />
       </Flex>
+      <ActionIcon
+        variant="filled"
+        aria-label="Github"
+        bg="black"
+        component={Link}
+        href="https://github.com/marcelldac?tab=repositories"
+        target="_blank"
+      >
+        <AiFillGithub />
+      </ActionIcon>
     </Flex>
   );
 }
