c8e0939507337d6727ea9eca67943c8fbe28b55f
marcelldac <dacmarcell@gmail.com>
2023-10-30 03:02:42 -0300

    feat: add site title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
-import { Html, Head, Main, NextScript } from "next/document";
+/* eslint-disable @next/next/no-title-in-document-head */
+import { Html, Main, NextScript, Head } from "next/document";
 import { ColorSchemeScript } from "@mantine/core";
 
 export default function Document() {
@@ -6,6 +7,8 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <ColorSchemeScript defaultColorScheme="auto" />
+        <title>Bubble Sort</title>
+        <meta property="og:title" content="Bubble Sort" key="Bubble Sort" />
       </Head>
       <body>
         <Main />
